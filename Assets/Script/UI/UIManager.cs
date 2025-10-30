316f354b2a04bdf865d1872d1d6f519606b5226d
JeongHyun <wlsrurdmlwjdrnf@gmail.com>
2025-10-30 13:16:50 +0900

    UI매니저 수정

---
+++ Assets/Script/UI/UIManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager ins;
+    public TextMeshProUGUI roundCount;
+    public TextMeshProUGUI goldCount;
+    public TextMeshProUGUI fieldUnit;
+
+    private void Awake()
+    {
+        if (ins == null)
+        {
+            ins = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void UpdateRound(int num)
+    {
+        roundCount.text = num.ToString();
+    }
+    public void UpdateGold(int num)
+    {
+        goldCount.text = num.ToString();
+    }
+    public void UpdateFieldUnit(int num)
+    {
+        fieldUnit.text = num.ToString() + " / 100";
+    }
+}
